14a200e3f5037b895092780e9a5015dea2846ffe
KaonWry <titan2011official@gmail.com>
2025-06-25 07:39:09 +0700

    Coba cara bagi tiap komponen ke file terpisah dan digabung di page.js

---
+++ app/components/section1.js
@@ -0,0 +1,11 @@
+const Section1 = () => {
+  return (
+    <section className="bg-amber-400 text-4xl mx-auto w-[100vw] h-[100vh]">
+      <div className="h-[100%] flex items-center justify-center">
+        <p>This is section 1. This is section 1</p>
+      </div>
+    </section>
+  );
+};
+
+export default Section1;
---
+++ app/components/section2.js
@@ -0,0 +1,11 @@
+const Section2 = () => {
+  return (
+    <section className="bg-emerald-300 text-4xl mx-auto w-[100vw] h-[100vh]">
+      <div className="h-[100%] flex items-center justify-center">
+        <p>This is section 2. This is section 2</p>
+      </div>
+    </section>
+  );
+};
+
+export default Section2;
