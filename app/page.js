f9d04eb66b5d424e71247617fd295970cc744531
KaonWry <titan2011official@gmail.com>
2025-06-30 01:58:51 +0700

    Create name input, create gradient for the darker section

--- app/page.js
+++ app/page.js
@@ -13,6 +13,20 @@ export default function Home() {
       <section className="sections h-screen bg-[#F5ECD5]">
         <div className="sections-content">
           <p>
+            Kita boleh kenalan dulu gak? <br />
+            Tulis namamu di samping kanan ini dong{" "}
+            <input
+              type="text"
+              className="text-2xl border-b cursor-pointer"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </p>
+        </div>
+      </section>
+      <section className="sections h-screen bg-[#F5ECD5]">
+        <div className="sections-content">
+          <p>
             Hai <span className="font-bold">{inputValue}</span>
           </p>
           <p>apa kabar?</p>
@@ -25,17 +39,18 @@ export default function Home() {
       </section>
       <section className="sections h-screen bg-[#F5ECD5]">
         <div className="sections-content">
-          <p>
-            Atau kamu merasa ada yang salah dalam
-            <br />
-          </p>
+          <p>Atau kamu merasa ada yang salah dalam</p>
         </div>
       </section>
+      {/* klo g butuh gradient ini hapus aja */}
+      <div className="bg-gradient-to-b from-[#F5ECD5] to-[#222222] h-[200vh] w-full"></div>
       <section className="sections h-[200vh] bg-[#222222] text-white">
         <div className="sections-content">
           <RandomFontText text={`Hidupmu`} />
         </div>
       </section>
+      {/* sama ini juga hapus klo g butuh, cuma gw suka aja si klo ada gradient */}
+      <div className="bg-gradient-to-b from-[#222222] to-[#F5ECD5] h-[200vh] w-full"></div>
       <section className="sections h-screen bg-[#F5ECD5]">
         <div className="sections-content">
           <p>Di sini, kamu akan mempelajari sesuatu yang baru</p>
