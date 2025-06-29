56e588e980e582730be49b00976200b22f95e382
KaonWry <titan2011official@gmail.com>
2025-06-29 08:53:20 +0700

    fix footer styling

--- app/components/footer.js
+++ app/components/footer.js
@@ -2,13 +2,16 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="w-screen">
       <div className="footer">
         <div className="text-xl">
           <p>Thank you</p>
-          <p>Inspired by : I Was Wrong, but, Go Made Me Right</p>
+          <p>Inspired by : <span className="font-bold">I Was Wrong, But God Made Me Right</span></p>
           <p>Made with ❤️ by Kelompok 3</p>
-          <p>&copy; {new Date().getFullYear()} Your Needs Everyday. All rights reserved.</p>
+          <p>
+            &copy; {new Date().getFullYear()} Your Needs Everyday. All rights
+            reserved.
+          </p>
         </div>
         <a
           href="https://github.com/KaonWry/your-needs-everyday"
