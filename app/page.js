0195a537dbe9a4363868ae51464b3fd404bd9c82
KaonWry <titan2011official@gmail.com>
2025-06-30 17:09:33 +0700

    Clean file formatting

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Footer from "./components/footer";
 import "./globals.css";
 import Login from "./loginpage";
 
@@ -9,22 +8,21 @@ export default function Home() {
   const [input, setInput] = useState("");
   const [fadeIn, setFadeIn] = useState(false);
   const router = useRouter();
-  
+
   useEffect(() => {
     setFadeIn(true);
   }, []);
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (input.trim()) {
       router.push(`/main?name=${encodeURIComponent(input)}`);
     }
   };
-  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <Login />
     </main>
   );
 }
-
