858488b03f3f0b0d280dcfc2314e8b5892d87475
Gimshi <gideonms99@gmail.com>
2025-06-30 22:36:38 +0700

    akhirnya selesai juga

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -26,53 +26,55 @@ const Navbar = ({ isMuted, toggleMute, volume, setVolume }) => {
   };
 
   return (
-    <nav className="navbar-container fixed bottom-0 w-full h-24 z-10">
-      <div className="flex items-center justify-between w-full h-full px-6">
-        <div />
-        <div className="flex items-center gap-4 relative">
+    <nav className="navbar-container fixed bottom-0 w-full h-24 z-10 flex items-center">
+      <div className="w-full flex items-center justify-end px-8">
+        <div className="flex items-center gap-4">
           <div
             onMouseEnter={handleSliderMouseEnter}
             onMouseLeave={handleSliderMouseLeave}
             className="relative flex items-center justify-center"
-            style={{ width: 32, height: 32 }}
+            style={{ width: 40, height: 40 }}
           >
             <button
               onClick={toggleMute}
-              className="navbar-icon-btn transition hover:scale-110"
+              className="navbar-icon-btn flex items-center justify-center bg-white/80 shadow-md"
               aria-label="Mute/Unmute"
-              style={{ background: 'none', border: 'none', padding: 0, margin: 0 }}
+              style={{ border: 'none', padding: 0, margin: 0, width: 40, height: 40, borderRadius: '50%' }}
             >
               {isMuted || volume === 0 ? (
-                <Image src="/mute.png" alt="Mute" width={30} height={24} />
+                <Image src="/mute.png" alt="Mute" width={24} height={24} />
               ) : (
-                <Image src="/unmute.png" alt="Unmute" width={30} height={24} />
+                <Image src="/unmute.png" alt="Unmute" width={24} height={24} />
               )}
             </button>
             {showSlider && (
               <div
-                className="navbar-slider-popup absolute bottom-12 right-0 flex flex-col items-center transition-opacity duration-300 opacity-100 pointer-events-auto"
+                className="navbar-slider-popup absolute bottom-12 left-1/2 -translate-x-1/2 flex flex-col items-center transition-opacity duration-300 opacity-100 pointer-events-auto"
                 style={{ height: "120px", zIndex: 20 }}
               >
-                <input
-                  type="range"
-                  min={0}
-                  max={1}
-                  step={0.01}
-                  value={isMuted ? 0 : volume}
-                  onChange={handleVolumeChange}
-                  className="vertical-slider accent-blue-500"
-                  aria-label="Volume"
-                  style={{
-                    writingMode: "vertical-lr",
-                    WebkitAppearance: "slider-vertical",
-                    MozAppearance: "slider-vertical",
-                    appearance: "slider-vertical",
-                    width: "32px",
-                    height: "100px",
-                    margin: 0,
-                    transform: "rotate(180deg)",
-                  }}
-                />
+                <div className="vertical-slider-wrapper">
+                  <div className="vertical-slider-bg"></div>
+                  <input
+                    type="range"
+                    min={0}
+                    max={1}
+                    step={0.01}
+                    value={isMuted ? 0 : volume}
+                    onChange={handleVolumeChange}
+                    className="vertical-slider"
+                    aria-label="Volume"
+                    style={{
+                      writingMode: "vertical-lr",
+                      WebkitAppearance: "slider-vertical",
+                      MozAppearance: "slider-vertical",
+                      appearance: "slider-vertical",
+                      width: "18px",
+                      height: "100px",
+                      margin: 0,
+                      transform: "rotate(180deg)"
+                    }}
+                  />
+                </div>
               </div>
             )}
           </div>
